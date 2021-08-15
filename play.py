21b593d908a4c1e723648d76c2896fd13d2ba760
sashakrippa <88389293+sashakrippa@users.noreply.github.com>
2021-08-16 02:09:22 +0300

    Update play.py

--- play.py
+++ play.py
@@ -23,3 +23,8 @@ while True:
             print("Scissors cuts paper! You win!")
         else:
             print("Rock smashes scissors! You lose.")
+            
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() != "y":
+                break
+
